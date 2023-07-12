bc79a122a35ee3e0f07a4c0f38fb94c0c4f34b50
bernie6401 <bernie6401@gmail.com>
2023-07-12 17:19:04 +0800

    update php deobfuscator

---
+++ Reverse/PHP-Deobfuscator/PHP-Deobfuscator/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Proj
+{
+    // Fixes obfuscation pattern of the form ('['^'(').(')'^']')
+    public static string RemoveParanthesisPattern(string data)
+    {
+        string pattern = @"\(('.'\^)+'.'\)"; // ('['^':')
+
+        Regex regex = new Regex(pattern);
+        MatchCollection matches = regex.Matches(data);
+
+        int found = 0;
+        foreach (Match match in matches)
+        {
+            // Console.WriteLine(match);
+            // Console.WriteLine("----------------------------");
+
+            // Removing the first two characters ('
+            string tmp = match.ToString().Remove(0, 1).Remove(0, 1);
+            // Removing the last two characters ')
+            tmp = tmp.Remove(tmp.Length - 2, 1).Remove(tmp.Length - 2, 1);
+            tmp = tmp.Replace("'^'", "\x01");
+
+            int val = 0;
+            foreach (var x in tmp.Split('\x01', StringSplitOptions.None))
+            {
+                val ^= char.Parse(x);
+            }
+
+            data = data.Replace(match.ToString(), ((char)val).ToString());
+            found += 1;
+        }
+
+        Console.WriteLine($"RemoveParanthesisPattern: Total Matches Found: {found}");
+        return data;
+    }
+
+    // Fixes names of the form s.t.r.s.t.r --> strstr
+    public static string FixDottedNames(string data, string pattern = "")
+    {
+        // XXX: Use this pattern if something breaks
+        // string pattern = @"\((([a-z])\.)+[a-z]\)";
+        pattern = pattern == "" ? @"\((([a-z0-9_])\.)+[a-z]\)" : pattern;
+
+        Regex regex = new Regex(pattern);
+        MatchCollection matches = regex.Matches(data);
+
+        int found = 0;
+        foreach (Match match in matches)
+        {
+            string tmp = match.ToString();
+            if (tmp.StartsWith("("))
+            {
+                tmp = tmp.Remove(tmp.Length - 1, 1).Remove(0, 1);
+            }
+            tmp = tmp.Replace(".", "");
+
+            data = data.Replace(match.ToString(), tmp);
+            found += 1;
+        }
+
+        Console.WriteLine($"FixDottedNames: Total Matches Found: {found}");
+        return data;
+    }
+
+    static void Main(string[] args)
+    {
+        string filename = "obfuscated.php";
+        string fileData = File.ReadAllText(filename);
+
+        fileData = RemoveParanthesisPattern(fileData);
+        fileData = FixDottedNames(fileData);
+
+        // Step3
+        fileData = fileData.Replace(@"''.abs(strstr('','.'))", "'0'");
+
+        // Step4
+        fileData = RemoveParanthesisPattern(fileData);
+
+        fileData = FixDottedNames(fileData);
+        fileData = FixDottedNames(fileData, @"(([a-z0-9A-Z_])\.)+[a-z0-9A-Z]");
+
+        fileData = fileData.Replace(".'0'.", "0");
+
+        File.WriteAllText("deobfuscated.php", "<?php\n" + fileData);
+
+    }
+}
---
+++ Reverse/PHP-Deobfuscator/README.md
@@ -0,0 +1,2 @@
+# How to use?
+Put your obfuscated php file down in `PHP-Deobfuscator\bin\Debug\net6.0` this path(be careful the name must be `obfuscated.php`) and open `PHP-Deobfuscator.sln` with Visual Studio and just run the compiler directly. Then you'll find a deobfuscated file named `deobfuscated.php` at the same place with `obfuscated.php`.
